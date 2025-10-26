a37aaa86ed3f3b19e9bf12e7a46c923f782fee0f
Arsentiy Vavilov <vavilov_a@tochka.com>
2025-10-26 20:03:32 +0300

    Написал тест на страницу Enterprize

---
+++ src/test/java/LoadEnterprizePageTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+/**
+ * @author Arsentiy Vavilov
+ */
+class LoadEnterprizePageTests {
+    @Test
+    void correctLoadEnterprizePage() {
+        open("https://github.com/");
+        $(withText("Solutions")).hover();
+        $("a[href='https://github.com/enterprise']").click();
+        $(".application-main ").shouldHave(text("The AI-powered developer platform"));
+    }
+}
